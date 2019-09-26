ea88a027310d1f36f5a6e2c7b69f1db352da9abd
jimCresswell <jim.cresswell@gmail.com>
2019-09-26 15:47:49 +0100

    Wait for the search term box to be interactable

--- src/test/java/serenity_demo/interactions/search/SearchForSteps.java
+++ src/test/java/serenity_demo/interactions/search/SearchForSteps.java
@@ -7,7 +7,9 @@ public class SearchForSteps extends UIInteractionSteps {
 
     @Step("Search for term {0}")
     public void term(String term) {
-        $(SearchFormPageElement.SEARCH_FIELD).clear();
+        $(SearchFormPageElement.SEARCH_FIELD)
+                .waitUntilClickable()
+                .clear();
         $(SearchFormPageElement.SEARCH_FIELD).type(term);
         $(SearchFormPageElement.SEARCH_BUTTON).click();
     }
